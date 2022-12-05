7a210fd6750f4809a8ef8f51f75ad06994f329f5
Oleh-Semenenko <o.semenenko.mar@gmail.com>
2022-12-05 14:36:58 +0200

    add starts files

---
+++ src/components/App/App.js
@@ -0,0 +1,76 @@
+import { Component } from 'react';
+import ContactForm from '../ContactForm';
+import ContactList from '../ContactList';
+import Filter from '../Filter';
+import { nanoid } from 'nanoid';
+import { Container, Title, SecondaryTitle } from './App.styled';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    if (this.checkContactName(newContact.name)) {
+      alert(`${newContact.name} is already in contacts`);
+      return newContact.name;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  checkContactName = newName => {
+    return this.state.contacts.find(({ name }) => name === newName);
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const filteredContacts = this.getVisibleContacts();
+    
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+
+        <SecondaryTitle>Contacts</SecondaryTitle>
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
